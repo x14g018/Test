f6f1ed6aa0745e54f3f510e8f36c814a63723af2
x14g018 <x14g018@PC505N017.chiba-fjb.ac.jp>
2015-04-16 15:01:17 +0900

    応用

---
+++ src/Format.java
@@ -0,0 +1,42 @@
+
+public class Format {
+
+	public static void main(String[] args) {
+//		int a = 10;
+//		float b = 100.5f;
+//		double c = 1000.15;
+//		char d = 'M';
+//		String e = "ふぉ";
+//
+//		String s = String.format("整数:%05d\n小数:%f\n小数:%4.3f\n文字:%c\n文字列:%s\n",a,b,c,d,e);
+//		System.out.println(s);
+//
+//		String w = "AbCdE";
+//		//小文字
+//		System.out.println(w.toLowerCase());
+//		//大文字
+//		System.out.println(w.toUpperCase());
+
+//		String s = "ABCDEFABCDEF";
+//		//CDをXYに置換
+//		String s2 = s.replaceAll("CD", "XY");
+//		System.out.println(s2);
+//		System.out.println("こんにちは世界");
+//		System.out.print("こんにちは世界\\n");
+//		 System.err.print("こんにちは世界2\n");
+//		int i;
+//		//iを0に初期設定;i<3を満たしていたら処理を実行;処理終了後、iに1を足す
+//		for(i=0;i<3;i=i+1)  
+//		    System.out.println(i);  //iの値を出力し改行
+		
+
+//		int[] w = {5,8,2,1,4};
+//		int i;
+//		for(i=0;i<w.length;i++)
+//		{
+//		    System.out.println(w[i]);
+//		}
+		
+	}
+
+}
